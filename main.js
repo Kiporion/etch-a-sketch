663a3b2c10b190798765eeecb92ad7fcd7bbe213
Kiporion <meho.mustafic@outlook.com>
2023-01-23 18:29:51 +0100

    finished board, working on buttons

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
-document.addEventListener("DOMContentLoaded", function(){
+let color = 'black';
+document.addEventListener('DOMContentLoaded', function () {
   createBoard(16);
-  console.log("hi");
+
+  let btn_popup = document.querySelector('#popup');
+  btn_popup.addEventListener('click', function () {
+    let size = getSize();
+    createBoard(size);
+  });
+  console.log('hi');
 });
 
-function createBoard(size){
-  let board=document.querySelector(".board");
+function createBoard(size) {
+  let board = document.querySelector('.board');
 
-  board.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
-  board.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  let numDivs=size*size;
+  let numDivs = size * size;
 
-  for(let i=0; i<numDivs; i++){
-    let div=document.createElement("div");
-    div.style.background="yellow";
-    board.insertAdjacentElement("beforeEnd",div);
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement('div');
+    div.addEventListener('mouseover', colorDiv);
+    board.insertAdjacentElement('beforeEnd', div);
   }
+}
+
+function getSize() {
+  let input = prompt('What will be the size of the board?');
+  let message = document.querySelector('#message');
+  if (input == '') {
+    message.textContent = 'Please provide a number';
+  } else if (input <= 0 || input > 100) {
+    message.textContent = 'Provide a number between 1 and 100';
+  } else {
+    message.textContent = 'Now you can play.';
+    return input;
+  }
+}
+function colorDiv() {
+  if (color == 'random') {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = `black`;
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+  let divs = document.querySelectorAll('div');
+  divs.forEach((div) => (div.style.backgroundColor = 'white'));
 }
